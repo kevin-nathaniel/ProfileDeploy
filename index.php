481c72063702deccceaaf79ecb23eaa668b5a5bb
Roxes619 <jhokkejunior857@gmail.com>
2023-06-19 17:32:31 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,323 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- Boostrap -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+
+  <!-- Icon -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+
+  <!-- CSS -->
+  <link rel="stylesheet" href="style.css" />
+
+  <title>My Portofolio | Jo Kevin Nathaniel</title>
+</head>
+
+<body id="container">
+  <!-- Navbar -->
+  <nav class="navbar navbar-expand-lg navbar-light bg-primary shadow-sm fixed-top">
+    <div class="container">
+      <a class="navbar-brand" href="#">Jo Kevin Nathaniel</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#container">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#about">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#portofolio">Portofolio</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#blog">Blog</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#team">Team</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <!-- Akhir Navbar -->
+
+  <!-- Jumbotron -->
+  <section class="jumbotron text-center">
+    <img src="img/public/asset/Untitled-1.jpg" alt="Jo Kevin Nathaniel" width="200"
+      class="rounded-circle img-thumbnail">
+    <h1 class="display-4">Jo Kevin Nathaniel</h1>
+    <p class="lead">Programmer | Graphic Designer | Content Creator</p>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+      <path fill="#ffffff" fill-opacity="10"
+        d="M0,160L40,160C80,160,160,160,240,160C320,160,400,160,480,149.3C560,139,640,117,720,112C800,107,880,117,960,144C1040,171,1120,213,1200,202.7C1280,192,1360,128,1400,96L1440,64L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z">
+      </path>
+    </svg>
+  </section>
+  <!-- Akhir Jumbotron -->
+
+  <!-- About -->
+  <section id="about">
+    <div class="container">
+      <div class="row text-center mb-3">
+        <div class="col">
+          <div class="h2">About Me</div>
+        </div>
+      </div>
+      <div class="row justify-content-center fs-5 text-center">
+        <div class="col-md-4">
+          <p>My name is Jo Kevin Nathaniel. People always call me Kevin or Joke. I live in Balikpapan. I’m 19 years old.
+          </p>
+        </div>
+        <div class="col-md-4">
+          <p>I'm a tech enthusiast, mechanical keyboard modder, music instrument player, and i love sport.</p>
+        </div>
+      </div>
+    </div>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+      <path fill="#e2edff" fill-opacity="1"
+        d="M0,320L24,304C48,288,96,256,144,234.7C192,213,240,203,288,186.7C336,171,384,149,432,165.3C480,181,528,235,576,250.7C624,267,672,245,720,218.7C768,192,816,160,864,160C912,160,960,192,1008,208C1056,224,1104,224,1152,229.3C1200,235,1248,245,1296,234.7C1344,224,1392,192,1416,176L1440,160L1440,320L1416,320C1392,320,1344,320,1296,320C1248,320,1200,320,1152,320C1104,320,1056,320,1008,320C960,320,912,320,864,320C816,320,768,320,720,320C672,320,624,320,576,320C528,320,480,320,432,320C384,320,336,320,288,320C240,320,192,320,144,320C96,320,48,320,24,320L0,320Z">
+      </path>
+    </svg>
+  </section>
+  <!-- Akhir About -->
+
+  <!-- Portofolio -->
+  <section id="portofolio">
+    <div class="container">
+      <div class="row text-center mb-3">
+        <div class="col">
+          <h2>My Portofolio</h2>
+        </div>
+      </div>
+      <div class="row justify-content-center">
+        <div class="col-md-4 mb-3">
+          <div class="card">
+            <img src="portofolio/1.jpg" class="card-img-top" alt="Project 1">
+            <div class="card-body">
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                content.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 mb-3">
+          <div class="card">
+            <img src="portofolio/2.jpg" class="card-img-top" alt="Project 2">
+            <div class="card-body">
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                content.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 mb-3">
+          <div class="card">
+            <img src="portofolio/3.jpg" class="card-img-top" alt="Project 3">
+            <div class="card-body">
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                content.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 mb-3">
+          <div class="card">
+            <img src="portofolio/4.jpg" class="card-img-top" alt="Project 4">
+            <div class="card-body">
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                content.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 mb-3">
+          <div class="card">
+            <img src="portofolio/5.jpg" class="card-img-top" alt="Project 5">
+            <div class="card-body">
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                content.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+      <path fill="#ffffff" fill-opacity="1"
+        d="M0,0L24,26.7C48,53,96,107,144,106.7C192,107,240,53,288,48C336,43,384,85,432,90.7C480,96,528,64,576,64C624,64,672,96,720,122.7C768,149,816,171,864,160C912,149,960,107,1008,90.7C1056,75,1104,85,1152,117.3C1200,149,1248,203,1296,208C1344,213,1392,171,1416,149.3L1440,128L1440,320L1416,320C1392,320,1344,320,1296,320C1248,320,1200,320,1152,320C1104,320,1056,320,1008,320C960,320,912,320,864,320C816,320,768,320,720,320C672,320,624,320,576,320C528,320,480,320,432,320C384,320,336,320,288,320C240,320,192,320,144,320C96,320,48,320,24,320L0,320Z">
+      </path>
+    </svg>
+  </section>
+  <!-- Akhir Portofolio -->
+
+  <!-- Blog -->
+  <section id="blog">
+    <div class="container">
+      <div class="row text-center mb-3">
+        <div class="col">
+          <h2>My Blog</h2>
+        </div>
+      </div>
+      <div class="row justify-content-center">
+        <div class="col-md-3 mb-3">
+          <div class="card">
+            <img src="blog/1.jpg" class="card-img-top" alt="Blog 1">
+            <div class="card-body">
+              <h5 class="card-title">Blog 1</h5>
+              <p class="card-text">Photo by <a
+                  href="https://unsplash.com/@glenncarstenspeters?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Glenn
+                  Carstens-Peters</a> on <a
+                  href="https://unsplash.com/s/photos/blog?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+              </p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3 mb-3">
+          <div class="card">
+            <img src="blog/2.jpg" class="card-img-top" alt="Blog 2">
+            <div class="card-body">
+              <h5 class="card-title">Blog 2</h5>
+              <p class="card-text">Photo by <a
+                  href="https://unsplash.com/@glenncarstenspeters?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Glenn
+                  Carstens-Peters</a> on <a
+                  href="https://unsplash.com/s/photos/blog?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+              </p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3 mb-3">
+          <div class="card">
+            <img src="blog/3.jpg" class="card-img-top" alt="Blog 3">
+            <div class="card-body">
+              <h5 class="card-title">Blog 3</h5>
+              <p class="card-text">Photo by <a
+                  href="https://unsplash.com/@glenncarstenspeters?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Glenn
+                  Carstens-Peters</a> on <a
+                  href="https://unsplash.com/s/photos/blog?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+              </p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+      <path fill="#e2edff" fill-opacity="1"
+        d="M0,128L48,133.3C96,139,192,149,288,133.3C384,117,480,75,576,96C672,117,768,203,864,208C960,213,1056,139,1152,122.7C1248,107,1344,149,1392,170.7L1440,192L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z">
+      </path>
+    </svg>
+  </section>
+  <!-- Akhir Blog -->
+
+  <!-- Team  -->
+  <section id="team">
+    <div class="containet">
+      <div class="row text-center mb-3">
+        <div class="col">
+          <h2>My Team</h2>
+        </div>
+      </div>
+      <div class="row justify-content-center fs-5 text-center">
+        <div class="col-md-2 mb-3">
+          <div class="circle">
+            <img src="team/icon-256x256.png" alt="Jo Kevin Nathaniel" width="200" class="rounded-circle img-thumbnail">
+            <div class="body">
+              <p>Jo</p>
+              <p class="lead">Manager</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-2 mb-3">
+          <div class="circle">
+            <img src="team/icon-256x256.png" alt="Jo Kevin Nathaniel" width="200" class="rounded-circle img-thumbnail">
+            <div class="body">
+              <p>Jhokke</p>
+              <p class="lead">Graphic Designer</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-2 mb-2">
+          <div class="circle">
+            <img src="team/icon-256x256.png" alt="Jo Kevin Nathaniel" width="200" class="rounded-circle img-thumbnail">
+            <div class="body">
+              <p>Kevin</p>
+              <p class="lead">Private Teacher</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-2 mb-3">
+          <div class="circle">
+            <img src="team/icon-256x256.png" alt="Jo Kevin Nathaniel" width="200" class="rounded-circle img-thumbnail">
+            <div class="body">
+              <p>Nathan</p>
+              <p class="lead">Head Marketing</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+      <path fill="#ffffff" fill-opacity="1"
+        d="M0,288L18.5,272C36.9,256,74,224,111,202.7C147.7,181,185,171,222,186.7C258.5,203,295,245,332,224C369.2,203,406,117,443,112C480,107,517,181,554,202.7C590.8,224,628,192,665,202.7C701.5,213,738,267,775,261.3C812.3,256,849,192,886,176C923.1,160,960,192,997,181.3C1033.8,171,1071,117,1108,101.3C1144.6,85,1182,107,1218,133.3C1255.4,160,1292,192,1329,186.7C1366.2,181,1403,139,1422,117.3L1440,96L1440,320L1421.5,320C1403.1,320,1366,320,1329,320C1292.3,320,1255,320,1218,320C1181.5,320,1145,320,1108,320C1070.8,320,1034,320,997,320C960,320,923,320,886,320C849.2,320,812,320,775,320C738.5,320,702,320,665,320C627.7,320,591,320,554,320C516.9,320,480,320,443,320C406.2,320,369,320,332,320C295.4,320,258,320,222,320C184.6,320,148,320,111,320C73.8,320,37,320,18,320L0,320Z">
+      </path>
+    </svg>
+  </section>
+  <!-- Akhir Team -->
+
+  <!-- Contact -->
+  <section id="contact">
+    <div class="container">
+      <div class="row text-center">
+        <div class="col">
+          <h2>Contact Me</h2>
+        </div>
+      </div>
+      <div class="row justify-content-center mb-3">
+        <div class="col-md-6">
+          <form>
+            <div class="mb-3">
+              <label for="name" class="form-label">Full Name</label>
+              <input type="text" class="form-control" id="name" aria-describedby="name" />
+            </div>
+            <div class="mb-3">
+              <label for="email" class="form-label">Email</label>
+              <input type="email" class="form-control" id="email" aria-describedby="email" />
+            </div>
+            <div class="mb-3">
+              <label for="pesan" class="form-label">Message</label>
+              <textarea class="form-control" id="pesan" rows="3"></textarea>
+            </div>
+            <button type="submit" class="btn btn-primary">Send</button>
+          </form>
+        </div>
+      </div>
+    </div>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+      <path fill="#0d6efd" fill-opacity="1"
+        d="M0,64L18.5,58.7C36.9,53,74,43,111,85.3C147.7,128,185,224,222,240C258.5,256,295,192,332,181.3C369.2,171,406,213,443,192C480,171,517,85,554,58.7C590.8,32,628,64,665,85.3C701.5,107,738,117,775,122.7C812.3,128,849,128,886,144C923.1,160,960,192,997,186.7C1033.8,181,1071,139,1108,138.7C1144.6,139,1182,181,1218,186.7C1255.4,192,1292,160,1329,138.7C1366.2,117,1403,107,1422,101.3L1440,96L1440,320L1421.5,320C1403.1,320,1366,320,1329,320C1292.3,320,1255,320,1218,320C1181.5,320,1145,320,1108,320C1070.8,320,1034,320,997,320C960,320,923,320,886,320C849.2,320,812,320,775,320C738.5,320,702,320,665,320C627.7,320,591,320,554,320C516.9,320,480,320,443,320C406.2,320,369,320,332,320C295.4,320,258,320,222,320C184.6,320,148,320,111,320C73.8,320,37,320,18,320L0,320Z">
+      </path>
+    </svg>
+  </section>
+  <!-- Akhir Contact -->
+
+  <!-- Footer -->
+  <footer class="bg-primary text-white text-center pb-5">
+    <p>Created with <i class="bi bi-heart-fill text-danger"></i> by <a
+        href="https://www.instagram.com/kevinnathaniel_10/?hl=en" class="text-white fw-bold">Jo Kevin Nathaniel</a></p>
+  </footer>
+  <!-- Akhir Footer -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous">
+  </script>
+</body>
+
+</html>
